2049073c050007d03cf654672139bcf4ba101efc
Kimbali <kimgarcianton@hotmail.com>
2023-04-28 13:29:02 +0200

    add error handler on the server

--- back/server.js
+++ back/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -14,14 +15,11 @@ app.get('/', (req, res) => {
   res.send('API is runnnig fast');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(prod => prod._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5003;
 app.listen(
