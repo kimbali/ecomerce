a36d6264fa768750e7a46326060502e0c1e3cb94
Kimbali <kimgarcianton@hotmail.com>
2023-05-05 09:32:39 +0200

    feat(front): :sparkles: login screen

--- front/src/store.js
+++ front/src/store.js
@@ -6,20 +6,22 @@ import {
   productListReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
-// const userInfoFromStorage = localStorage.getItem('userInfo')
-//   ? JSON.parse(localStorage.getItem('userInfo'))
-//   : null;
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 
 // const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
 //   ? JSON.parse(localStorage.getItem('shippingAddress'))
@@ -30,7 +32,7 @@ const initialState = {
     cartItems: cartItemsFromStorage,
     // shippingAddress: shippingAddressFromStorage,
   },
-  // userLogin: { userInfo: userInfoFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
