6d9a6b45f99a83c6c39feda17ddce6e35c6b795d
Kimbali <kimgarcianton@hotmail.com>
2023-05-08 10:45:45 +0200

    feat(front): :sparkles: payment screen and checkout steps component

--- front/src/screens/ShippingScreen.js
+++ front/src/screens/ShippingScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
-// import CheckoutSteps from '../components/CheckoutSteps';
+import CheckoutSteps from '../components/CheckoutSteps';
 import { saveShippingAddress } from '../actions/cartActions';
 import { useNavigate } from 'react-router-dom';
 
@@ -25,7 +25,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
-      {/* <CheckoutSteps step1 step2 /> */}
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className='mb-4' controlId='address'>
