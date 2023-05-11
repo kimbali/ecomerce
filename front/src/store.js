e6dbb356e3ef5507afcc78c71c708f06135acf71
Kimbali <kimgarcianton@hotmail.com>
2023-05-11 11:22:21 +0200

    feat(FE/BE): :sparkles: create and update products

--- front/src/store.js
+++ front/src/store.js
@@ -2,16 +2,21 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import {
-  productDetailsReducer,
   productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+  productReviewCreateReducer,
+  productTopRatedReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
-  userDetailsReducer,
-  userListReducer,
   userLoginReducer,
   userRegisterReducer,
+  userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
   userDeleteReducer,
   userUpdateReducer,
 } from './reducers/userReducers';
@@ -27,6 +32,11 @@ import {
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
+  productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
