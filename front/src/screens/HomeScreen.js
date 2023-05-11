55260731d6229ae4a62bef5c5075f09d8006a0cf
Kimbali <kimgarcianton@hotmail.com>
2023-05-11 19:12:51 +0200

    feat(FE/BE): :sparkles: carousel with top rated products

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Paginate from '../components/Paginate';
-// import ProductCarousel from '../components/ProductCarousel';
+import ProductCarousel from '../components/ProductCarousel';
 // import Meta from '../components/Meta';
 import { listProducts } from '../actions/productActions';
 
@@ -27,8 +27,7 @@ const HomeScreen = () => {
     <>
       {/* <Meta /> */}
       {!keyword ? (
-        // <ProductCarousel />
-        <h2>Caroutsel</h2>
+        <ProductCarousel />
       ) : (
         <Link to='/' className='btn btn-light'>
           Go Back
